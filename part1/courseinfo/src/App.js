e3a7c316133642e768e396ddf50ccfc55de24d9e
LekishviliRati <lekishvili.rati@gmail.com>
2022-08-19 09:01:56 +0200

    1.5 Done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-/*
+/* Exercise 1.3
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
@@ -25,7 +25,7 @@ const App = () => {
   )
 }
 */
-
+/* Exercise 1.4
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -50,6 +50,38 @@ const App = () => {
      <p> Name of part2 : {parts[1].name}. Number of exercises : {parts[1].exercises} </p>
      <p> Name of part3 : {parts[2].name}. Number of exercises : {parts[2].exercises} </p>
      <p> Total of exercises : {parts[0].exercises + parts[1].exercises + parts[2].exercises} </p>
+    </div>
+  )
+}
+*/
+
+/* Exercise 1.5 */
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+     <p> Name of the course : {course.name} </p> 
+     <p> Name of part1 : {course.parts[0].name}. Number of exercises : {course.parts[0].exercises} </p>
+     <p> Name of part2 : {course.parts[1].name}. Number of exercises : {course.parts[1].exercises} </p>
+     <p> Name of part3 : {course.parts[2].name}. Number of exercises : {course.parts[2].exercises} </p>
+     <p> Total of exercises : {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} </p>
     </div>
   )
 }
